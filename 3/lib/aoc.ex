affd4f5c95deabf65467c011adab3bb91d824022
Rik Hemsley <rik@hemsley.cc>
2017-12-04 18:15:15 +0000

    Solved day 3 part 1

--- 3/lib/aoc.ex
+++ 3/lib/aoc.ex
@@ -1,4 +1,93 @@
 defmodule AOC do
-  # TODO: implement stuff 
+
+  def manhattan_distance_from_origin({x, y}) do
+    abs(x) + abs(y)
+  end
+
+  def foo({x, y}, {nx, ny}, {xrem, yrem}, {dx, dy}, n, target, steps) do
+    steps = Map.put(steps, {x, y}, n)
+
+    #print(steps)
+    #IO.inspect {:foo, %{pos: {x, y}, next: {nx, ny}, rem: {xrem, yrem}, d: {dx, dy}, n: n, target: target}}
+    case n == target do
+      true -> {{x, y}, steps}
+      false -> next_pos({x, y}, {nx, ny}, {xrem, yrem}, {dx, dy}, n + 1, target, steps)
+    end
+  end
+
+  # We've run out on this leg. Let's turn the corner.
+
+  def next_pos({x, y}, {nx, ny}, {0, 0}, {0, -1}, n, target, steps) do
+    #IO.inspect "We were going up. Let's go left now."
+    foo {x, y - 1}, {nx, ny + 1}, {nx, 0}, {-1, 0}, n, target, steps
+  end
+
+  def next_pos({x, y}, {nx, ny}, {0, 0}, {-1, 0}, n, target, steps) do
+    #IO.inspect "We were going left. Let's go down now."
+    foo {x - 1, y}, {nx + 1, ny}, {0, ny}, {0, 1}, n, target, steps
+  end
+
+  def next_pos({x, y}, {nx, ny}, {0, 0}, {0, 1}, n, target, steps) do
+    #IO.inspect "We were going down. Let's go right now."
+    foo {x, y + 1}, {nx, ny + 1}, {nx, 0}, {1, 0}, n, target, steps
+  end
+
+  def next_pos({x, y}, {nx, ny}, {0, 0}, {1, 0}, n, target, steps) do
+    #IO.inspect "We were going right. Let's go up now."
+    foo {x + 1, y}, {nx + 1, ny}, {0, ny}, {0, -1}, n, target, steps
+  end
+
+  # We're just counting down - so let's continue, after subtracting one.
+
+  def next_pos({x, y}, {nx, ny}, {xrem, 0}, {dx, dy}, n, target, steps) do
+    #IO.inspect "We were going {#{dx}, #{dy}}. Let's continue but subtract from xrem."
+    foo {x + dx, y + dy}, {nx, ny}, {xrem - 1, 0}, {dx, dy}, n, target, steps
+  end
+
+  def next_pos({x, y}, {nx, ny}, {0, yrem}, {dx, dy}, n, target, steps) do
+    #IO.inspect "We were going {#{dx}, #{dy}}. Let's continue but subtract from yrem."
+    foo {x + dx, y + dy}, {nx, ny}, {0, yrem - 1}, {dx, dy}, n, target, steps
+  end
+
+  def co_ordinates(target) do
+    {{x, y}, steps} = foo({0, 0}, {0, 0}, {0, 0}, {1, 0}, 1, target, MapSet.new)
+  end
+
+  def solve(input, :one) do
+    IO.inspect {:solve, input}
+    {{x, y}, steps} = co_ordinates(input)
+
+
+    #print(steps)
+
+    {x, y} |> manhattan_distance_from_origin
+  end
+
+  def solve(input, :two) do
+    raise "Not implemented"
+  end
+
+  def print_pos(steps, x, y) do
+
+    character = case Map.get(steps, {x, y}) do
+      nil -> " .. "
+      n -> " " <> String.pad_leading(Integer.to_string(n), 2, " ") <> " "
+    end
+
+    IO.write(character)
+  end
+ 
+  def print_row(steps, y) do
+    Enum.each(-2..2, fn(x) -> print_pos(steps, x, y) end)
+  end
+
+  def print(steps) do
+    Enum.each(-2..2, fn(y) ->
+        print_row(steps, y)
+        IO.puts ""
+      end
+    )
+  end
+
 end
 
--- 3/test/aoc_test.exs
+++ 3/test/aoc_test.exs
@@ -2,23 +2,55 @@ defmodule AOCTest do
   use ExUnit.Case
   doctest AOC
 
-@test_input "
-" |> String.trim 
+  @year 2017
+  @day 3
+  @part_one_expected 326
+  @part_two_expected nil
 
-  @tag :skip
-  test "example" do
-    assert 1 == 1
+  test "example part one 1" do
+    assert AOC.solve(1, :one) == 0
   end
 
-  @tag :skip
-  @tag timeout: 6000000
+  test "example part one 12" do
+    assert AOC.solve(12, :one) == 3
+  end
+
+  test "example part one 23" do
+    assert AOC.solve(23, :one) == 2
+  end
+
+  test "example part one 1024" do
+    assert AOC.solve(1024, :one) == 31
+  end
+
   test "part one" do
-    assert 1 == 1
+    {:ok, input} = AdventOfCodeHelper.get_input(@year, @day)
+    assert AOC.solve(elem(Integer.parse(String.trim(input)), 0), :one) == @part_one_expected
+  end
+
+  test "example part two 1" do
+    assert AOC.solve(1, :two) == 1
+  end
+
+  test "example part two 2" do
+    assert AOC.solve(2, :two) == 1
+  end
+
+  test "example part two 3" do
+    assert AOC.solve(3, :two) == 2
+  end
+
+  test "example part two 4" do
+    assert AOC.solve(4, :two) == 4
+  end
+
+  test "example part two 5" do
+    assert AOC.solve(5, :two) == 5
   end
 
   @tag :skip
-  @tag timeout: 6000000
   test "part two" do
-    assert 1 == 1
+    {:ok, input} = AdventOfCodeHelper.get_input(@year, @day)
+    assert AOC.solve(elem(Integer.parse(String.trim(input)), 0), :two) == @part_two_expected
   end
 end
